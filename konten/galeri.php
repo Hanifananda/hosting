fc3d5758b869393a613c401bce5d68f736d1d789
khansa-nadhifa <59870203+khansa-nadhifa@users.noreply.github.com>
2020-01-17 12:59:32 +0700

    firts project

---
+++ konten/galeri.php
@@ -0,0 +1,5 @@
+Ini halaman galeri
+
+<p>
+    <img src="./images/persona.png" height="200">
+</p>
---
+++ konten/jadwal.php
@@ -0,0 +1,22 @@
+Ini halaman jadwal
+<p></p> <!-- sebagai enter !--> 
+<table width="377" border="1">
+    <tr>
+		<td width="22" bgcolor="ffd1dc">No.</td>
+		<td width="113" bgcolor="ffd1dc">Mata Kuliah</td>
+		<td width="65" bgcolor="ffd1dc">Hari</td>
+	    <td width="76" bgcolor="ffd1dc">Waktu</td>
+	</tr>
+	<tr>
+		<td>1.</td>
+		<td>Web Programming</td>
+		<td>Rabu</td>
+		<td>18.30 - 20.00</td>
+	</tr>
+	<tr>
+    	<td>2.</td>
+		<td>Bahasa Arab</td>
+		<td>Rabu</td>
+		<td>20.00 - 21.30</td>
+	</tr>
+</table>
